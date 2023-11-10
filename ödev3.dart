856a2b5996da4674cb4c5b420297e4fd7915f665
ThunderBolt <92175630+eYLuLulgen@users.noreply.github.com>
2023-11-10 17:20:39 +0300

    Update ödev3.dart

--- ödev3.dart
+++ ödev3.dart
@@ -1,16 +1,18 @@
-
 import 'dart:math';
 
 // generateRandom fonksiyonu, rastgele olarak 100 veya null döndürür
-int generateRandom() {
-  return Random().nextBool() ? 100 : 0;
+int? generateRandom() {
+  return Random().nextBool() ? 100 : null;
 }
 
 void main() {
-  // status değişkenini tanımlar.generateRandom fonksiyonunun dönüş değerini atar
-  int status = generateRandom();
+  // status değişkenini tanımlar. generateRandom fonksiyonunun dönüş değerini atar
+  int? status = generateRandom();
 
-  // Sonucu yazdırın
-  print('Status: $status');
+  // Eğer status null ise 0'ı yazdırır, değilse status'ı yazdırır
+  if (status == null) {
+    print("0");
+  } else {
+    print('100');
+  }
 }
-
